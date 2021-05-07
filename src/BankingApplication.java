ede6d67a52d104ad870937b39fbb3ce6567f7540
Dillon DeGarmo <cddegarmo@gmail.com>
2021-05-07 06:21:22 -0500

    Update client code

--- src/BankingApplication.java
+++ src/BankingApplication.java
@@ -2,10 +2,10 @@
 public class BankingApplication {
     public static void main(String[] args) {
 
-        BankAccount dillonChecking =
-            BankAccount.addAccount("Dillon DeGarmo","24601");
+        BankAccount checking =
+            BankAccount.addAccount("John Doe", "54367");
 
-        Checking ck = Checking.addAccount("Dillon DeGarmo", "24601");
+        Checking ck = Checking.addAccount("Jane Doe", "24601");
         ck.showMenu();
     }
 }
