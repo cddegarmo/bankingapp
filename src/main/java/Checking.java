dea1224dc34ffe01e7e77f5a574c8b8815230ccd
Dillon DeGarmo <cddegarmo@gmail.com>
2021-05-07 10:50:08 -0500

    Update checking to reflect changes in BankAccount

--- src/main/java/Checking.java
+++ src/main/java/Checking.java
@@ -1,18 +1,19 @@
+package main.java;
+
 public class Checking {
     private BankAccount ac;
 
-    private Checking(String cname, String cid) {
+    private Checking(String cname, int cid) {
         ac = BankAccount.addAccount(cname, cid);
     }
 
-    public static Checking addAccount(String cname, String cid) {
+    public static Checking addAccount(String cname, int cid) {
         return new Checking(cname, cid);
     }
 
-    public int getBalance()          { return ac.getBalance();          }
-    public String getCustomerName()  { return ac.getCustomerName();     }
-    public String getCustomerId()    { return ac.getCustomerId();       }
-
+    public int getBalance()              { return ac.getBalance();       }
+    public String getCustomerName()      { return ac.getCustomerName();  }
+    public int getAccountNumber()        { return ac.getAccountNumber(); }
     public void deposit(int amount)      { ac.deposit(amount);          }
     public void withdraw(int amount)     { ac.withdraw(amount);         }
     public void getPreviousTransaction() { ac.getPreviousTransaction(); }
